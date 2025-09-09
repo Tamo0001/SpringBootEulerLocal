5088b01ddb5d8d993ff50a74ae4813fe40ba0d03
Timo H <motodefy.hh@gmail.com>
2025-09-09 11:58:54 +0200

    Initial commit

---
+++ src/test/java/com/SpringBoot/Euler/EulerSpringApplication.java
@@ -0,0 +1,16 @@
+package com.SpringBoot.Euler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class EulerSpringApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(EulerSpringApplication.class, args);
+        var euler6 = context.getBean(Euler6.class);
+        euler6.sumSquaresResult();
+        System.out.println(euler6.sumSquaresResult());
+    }
+}
